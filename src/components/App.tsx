7abbb568ba87fdc64be19c61b00570fb6c215ce3
Jesus Borjas <jesusbor006@gmail.com>
2023-09-10 12:34:36 -0500

    Add routing functionality inside nav

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,15 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Shows from "../pages/Shows";
+import Navbar from "./Navbar";
 
 function App() {
   return (
-    <>
+    <div className="max-w-[1650px] py-5 px-10 mx-auto">
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/shows" element={<Shows />} />
       </Routes>
-    </>
+    </div>
   );
 }
 
