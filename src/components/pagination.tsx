57a5789b83756ecdca4d2f1773b15bcba85b2670
Jesus Borjas <jesusbor006@gmail.com>
2023-09-11 15:41:49 -0500

    Fix styling for component when on smaller devices

--- src/components/pagination.tsx
+++ src/components/pagination.tsx
@@ -5,11 +5,11 @@ type PaginationProps = {
 
 const Pagination = ({ pageNum, setPageNum }: PaginationProps) => {
   return (
-    <ul className="flex space-x-8 justify-center mt-20 mb-10">
+    <ul className="flex justify-between mt-20 mb-10 max-w-[350px] mx-auto">
       {pageNum !== 1 && (
         <li
           onClick={() => setPageNum((prev) => prev - 1)}
-          className="cursor-pointer border py-2 px-4 rounded-md"
+          className="cursor-pointer border py-2 px-3 rounded-md text-sm"
         >
           Prev
         </li>
@@ -19,8 +19,8 @@ const Pagination = ({ pageNum, setPageNum }: PaginationProps) => {
         onClick={() => setPageNum(1)}
         className={
           pageNum === 1
-            ? " bg-blue-500 text-white border py-2 px-4 rounded-md"
-            : " cursor-pointer border py-2 px-4 rounded-md"
+            ? " bg-blue-500 text-white border py-2 px-3 rounded-md text-sm"
+            : " cursor-pointer border py-2 px-3 rounded-md text-sm"
         }
       >
         1
@@ -29,8 +29,8 @@ const Pagination = ({ pageNum, setPageNum }: PaginationProps) => {
         onClick={() => setPageNum(2)}
         className={
           pageNum === 2
-            ? " bg-blue-500 text-white border py-2 px-4 rounded-md"
-            : "text-black cursor-pointer border py-2 px-4 rounded-md"
+            ? " bg-blue-500 text-white border py-2 px-3 rounded-md text-sm"
+            : " cursor-pointer border py-2 px-3 rounded-md text-sm"
         }
       >
         2
@@ -39,8 +39,8 @@ const Pagination = ({ pageNum, setPageNum }: PaginationProps) => {
         onClick={() => setPageNum(3)}
         className={
           pageNum === 3
-            ? " bg-blue-500 text-white border py-2 px-4 rounded-md "
-            : " cursor-pointer border py-2 px-4 rounded-md"
+            ? " bg-blue-500 text-white border py-2 px-3 rounded-md text-sm"
+            : " cursor-pointer border py-2 px-3 rounded-md text-sm"
         }
       >
         3
@@ -49,8 +49,8 @@ const Pagination = ({ pageNum, setPageNum }: PaginationProps) => {
         onClick={() => setPageNum(4)}
         className={
           pageNum === 4
-            ? " bg-blue-500 text-white border py-2 px-4 rounded-md "
-            : " cursor-pointer border py-2 px-4 rounded-md"
+            ? " bg-blue-500 text-white border py-2 px-3 rounded-md text-sm"
+            : " cursor-pointer border py-2 px-3 rounded-md text-sm"
         }
       >
         4
@@ -59,8 +59,8 @@ const Pagination = ({ pageNum, setPageNum }: PaginationProps) => {
         onClick={() => setPageNum(5)}
         className={
           pageNum === 5
-            ? " bg-blue-500 text-white border py-2 px-4 rounded-md "
-            : " cursor-pointer border py-2 px-4 rounded-md"
+            ? " bg-blue-500 text-white border py-2 px-3 rounded-md text-sm"
+            : " cursor-pointer border py-2 px-3 rounded-md text-sm"
         }
       >
         5
@@ -68,7 +68,7 @@ const Pagination = ({ pageNum, setPageNum }: PaginationProps) => {
       {pageNum !== 5 && (
         <li
           onClick={() => setPageNum((prev) => prev + 1)}
-          className="cursor-pointer border py-2 px-4 rounded-md"
+          className="cursor-pointer border py-2 px-3 rounded-md text-sm"
         >
           Next
         </li>
