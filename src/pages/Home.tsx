4e32199c0c7e685a6971219fb013b1b89778775b
Jesus Borjas <jesusbor006@gmail.com>
2023-09-11 12:02:25 -0500

    Fetch data from api inside home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
-  return <h1>Movies</h1>;
+  const [movies, setMovies] = useState(null);
+  console.log(movies);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await fetch(
+        "https://api.themoviedb.org/3/movie/now_playing?api_key=53208bbbe918239e53dd4f9602cf91b5"
+      );
+      const data = await response.json();
+      setMovies(data.results);
+    };
+    fetchMovies();
+  }, []);
+
+  return <main></main>;
 };
 
 export default Home;
