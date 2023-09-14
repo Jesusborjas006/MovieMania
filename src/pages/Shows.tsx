a775ffba8e0713dfba051df89116fb319919f2ea
Jesus Borjas <jesusbor006@gmail.com>
2023-09-14 13:56:07 -0500

    Add error handling to shows page

--- src/pages/Shows.tsx
+++ src/pages/Shows.tsx
@@ -3,6 +3,7 @@ import { ShowData } from "../types";
 import Pagination from "../components/pagination";
 import Show from "../components/Show";
 import Loading from "../components/Loading";
+import ErrorMessage from "../components/ErrorMessage";
 
 type ShowProps = {
   setSelectedShowID: React.Dispatch<React.SetStateAction<number>>;
@@ -13,6 +14,7 @@ type ShowProps = {
 const Shows = ({ setSelectedShowID, isLoading, setIsLoading }: ShowProps) => {
   const [shows, setShows] = useState<ShowData | []>([]);
   const [pageNum, setPageNum] = useState(1);
+  const [error, setError] = useState("");
 
   const showElements = shows.map((show) => (
     <Show
@@ -26,22 +28,30 @@ const Shows = ({ setSelectedShowID, isLoading, setIsLoading }: ShowProps) => {
 
   useEffect(() => {
     const fetchShows = async () => {
-      setIsLoading(true);
-      const response = await fetch(
-        `https://api.themoviedb.org/3/trending/tv/day?api_key=${process.env.REACT_APP_API_KEY}&page=${pageNum}`
-      );
-      const data = await response.json();
-      setShows(data.results);
-      setIsLoading(false);
+      try {
+        setIsLoading(true);
+        const response = await fetch(
+          `https://api.themoviedb.org/3/trending/tv/day?api_key=${process.env.REACT_APP_API_KEY}&page=0`
+        );
+        if (!response.ok) {
+          setIsLoading(false);
+          throw new Error("Something went wrong with fetching shows");
+        }
+
+        const data = await response.json();
+        setShows(data.results);
+        setIsLoading(false);
+      } catch (err: any) {
+        setError(err.message);
+      }
     };
     fetchShows();
   }, [pageNum, setIsLoading]);
 
   return (
     <div className="max-w-[1650px] py-5 px-4 md:px-10 mx-auto">
-      {isLoading ? (
-        <Loading />
-      ) : (
+      {isLoading && <Loading />}
+      {!isLoading && !error && (
         <>
           <main className="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 ">
             {showElements}
@@ -49,6 +59,7 @@ const Shows = ({ setSelectedShowID, isLoading, setIsLoading }: ShowProps) => {
           <Pagination pageNum={pageNum} setPageNum={setPageNum} />
         </>
       )}
+      {error && <ErrorMessage />}
     </div>
   );
 };
