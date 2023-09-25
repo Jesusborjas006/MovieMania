2993038730acf765a9e9892a276c77e1c3022e3b
Jesus Borjas <jesusbor006@gmail.com>
2023-09-25 14:08:18 -0500

    Fix styling issues when img is not found

--- src/components/Movie.tsx
+++ src/components/Movie.tsx
@@ -18,13 +18,13 @@ const Movie = ({ id, posterImg, title, setSelectedMovie }: MovieProps) => {
     >
       {posterImg ? (
         <img
-          className="mb-2 border-2 border-[#121212] rounded-md  hover:border-2 hover:border-white"
+          className="mb-2 border-2 border-[#121212] rounded-md  hover:border-2 hover:border-white h-[83%] w-full"
           src={`http://image.tmdb.org/t/p/w500/${posterImg}`}
           alt={title}
         />
       ) : (
         <img
-          className="h-[85%] object-cover mb-2 border-2 border-[#121212] rounded-md"
+          className=" mb-2 border-2 border-[#121212] rounded-md h-[83%] w-full object-cover"
           src={imgNotFound}
           alt="not found"
         />
--- src/components/Show.tsx
+++ src/components/Show.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import imgNotFound from "../assets/not-found-2.png";
 
 type ShowProps = {
   key: number;
@@ -17,11 +18,20 @@ const Show = ({ id, name, posterImg, setSelectedShowID }: ShowProps) => {
       className="xl:text-lg"
       onClick={() => setSelectedShowID(id)}
     >
-      <img
-        className="rounded-md mb-2 border-2 border-[#121212] hover:border-2 hover:border-white"
-        src={`http://image.tmdb.org/t/p/w500/${posterImg}`}
-        alt={name}
-      />
+      {posterImg ? (
+        <img
+          className="rounded-md mb-2 border-2 border-[#121212] hover:border-2 hover:border-white h-[83%] w-full"
+          src={`http://image.tmdb.org/t/p/w500/${posterImg}`}
+          alt={name}
+        />
+      ) : (
+        <img
+          className=" mb-2 border-2 border-[#121212] rounded-md h-[83%] w-full object-cover"
+          src={imgNotFound}
+          alt="not found"
+        />
+      )}
+
       <h2>{name}</h2>
     </Link>
   );
